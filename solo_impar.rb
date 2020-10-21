9df1a14d6d42a7617b2b583f6acd14f8df24dfb7
Gilibeth Vega <gvega@creditu.com>
2020-10-21 08:19:13 -0300

    se completa desafío 4

--- solo_impar.rb
+++ solo_impar.rb
@@ -1,11 +1,11 @@
 #Crea un programa llamado solo_impares.rb que dado n muestre en pantalla los primeros n números impares.
 
 i= 0
-n_par= ARGV[0].to_i
-pares= 0
+n_impar= ARGV[0].to_i
+impares= 1
 
-while i < n_par
+while i < n_impar
   i+=1
-  puts pares 
-  pares+=2
+  puts impares 
+  impares+=2
 end
