7c862b321b22fbd6e67f6a413f1afb64155174ac
Gilibeth Vega <gilibethvega@gmail.com>
2020-10-22 21:53:55 -0300

    se desarrolla desafío 8

---
+++ fuerzabruta.rb
@@ -0,0 +1,13 @@
+#Se busca crear un programa fuerza_bruta.rb que revise cuantos intentos requiere hackear un password por fuerza bruta.
+
+password = ARGV[0]
+letra = 'a' 
+i = 0
+valorsumado = 0
+
+while letra != password
+  letra = letra.next
+  valorsumado += 1
+end
+
+puts valorsumado
